65ed19b48dd6d8aefd1426e460fdccf8425c7af3
Se-star-ctrl <seyi.akinbowale@binghamuni.edu.ng>
2026-01-21 00:44:18 +0100

    Add files via upload

---
+++ final.py
@@ -0,0 +1,23 @@
+import streamlit as st
+hausa = {
+    "chair": "kujera",
+    "stone": "dutse",
+    "bag": "jaka",
+    "shoe": "takalmi",
+    "wrapper": "zani",
+    "phone": "waya",
+    "bed": "godo",
+    "food": "abichin"
+    "clothes": "kaya",
+    "plates": "kwanu",
+    "bottle": "gora" ,
+    "scissors": "almakachi",
+    "knife": "wuka",
+    "basket": "kwondo",
+    "house": "Gida",
+    "money": "kudi" ,
+    "car": "mota",
+    "water": "ruwa"
+   "horse": "doki",
+    "bone": "Kashi"
+}
